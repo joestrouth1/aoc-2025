6ceda9bc47a5d724c7291d8a94e14bdc376b9d24
Joe Strouth <joestrouth1@gmail.com>
2025-12-02 13:25:40 -0500

    day 2 pt 2

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -1,9 +1,20 @@
 use std::io;
 
+struct Score {
+    count: u64,
+    sum: u64,
+}
+
+impl Score {
+    fn add(&mut self, id: u64) {
+        self.count += 1;
+        self.sum += id;
+    }
+}
+
 fn main() {
     let stdin = io::stdin();
-    let mut invalid_id_count: u64 = 0;
-    let mut invalid_id_sum: u64 = 0;
+    let mut score = Score { count: 0, sum: 0 };
 
     let mut line = String::new();
     let _ = stdin.read_line(&mut line);
@@ -14,22 +25,33 @@ fn main() {
 
         let mut current_id = lower_bound;
         while current_id <= upper_bound {
-            let current_id_string = current_id.to_string();
-            if current_id_string.len() % 2 == 0 {
-                let midpoint = current_id_string.len() / 2;
-                let first_half = &current_id_string[..midpoint];
-                let second_half = &current_id_string[midpoint..];
-                if first_half == second_half {
-                    invalid_id_sum += current_id;
-                    invalid_id_count += 1;
+            let current_id_string = &current_id.to_string();
+            // pt 1
+            // if current_id_string.len() % 2 == 0 {
+            //     let midpoint = current_id_string.len() / 2;
+            //     let first_half = &current_id_string[..midpoint];
+            //     let second_half = &current_id_string[midpoint..];
+            //     if first_half == second_half {
+            //         score.add(current_id)
+            //     }
+            // }
+
+            // pt2
+            if current_id_string.len() >= 2 {
+                let slice_lengths_range = 1..(current_id_string.len() / 2 + 1);
+                let sliceable_lengths =
+                    slice_lengths_range.filter(|len| current_id_string.len() % len == 0);
+                for slice_length in sliceable_lengths.rev() {
+                    let first_slice = &current_id_string[..slice_length];
+                    let slice_matches: Vec<&str> = current_id_string.matches(first_slice).collect();
+                    if slice_matches.len() == (current_id_string.len() / slice_length) {
+                        score.add(current_id);
+                        break;
+                    }
                 }
             }
-
             current_id += 1;
         }
     }
-    println!(
-        "{} invalid IDs found, sum to: {}",
-        invalid_id_count, invalid_id_sum
-    )
+    println!("{} invalid IDs found, sum to: {}", score.count, score.sum)
 }
